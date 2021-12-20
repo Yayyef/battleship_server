366a33f90e1b80ff491128483a7ea7302414feb9
hugo <hugo@aa55.fr>
2021-12-20 13:17:50 +0100

    server class

---
+++ BS_Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace src
+{
+	public class BS_Server
+	{
+		private Socket passiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+		private Socket clientSocket;
+		private  byte[] data = new byte[1024];
+
+		private static int port = 1000;
+
+		public BS_Server()
+		{
+           
+        }
+
+        public void StartServer()
+        {
+         
+            passiveSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            passiveSocket.Bind(new System.Net.IPEndPoint(IPAddress.Any, port));
+            passiveSocket.Listen(1024);
+            Console.WriteLine("Searching for players...");
+            clientSocket = passiveSocket.Accept();
+            IPEndPoint EP = (IPEndPoint)clientSocket.RemoteEndPoint;
+            Console.WriteLine("A new challenger ! Connected to host " + EP.Address);
+
+
+        }
+
+
+        public void SendPosition(int X, int Y)
+        {
+
+        }
+
+        public void GetPosition(int *X, int *Y)
+        {
+
+        }
+
+        public ~BS_Server()
+        {
+
+            if (passiveSocket != null)
+            {
+                passiveSocket.Close();
+            }
+            if (clientSocket != null)
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+
+
+
+    }
+}
+
